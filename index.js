3549ca41ebed781e0d8495e4f7af00ec1822f302
weddfri <weddfri2@gmail.com>
2026-04-29 14:02:01 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function tombol1() {
+     window.open("https://wa.me/628136683171?text=Halo%20kak,%20saya%20tertarik%20dengan%20produk%20kakak.%0ASaya%20ingin%20memesan%20produk%20kakak%20ya.%0A%0ANama:%20...%0AAlamat:%20...%0AJumlah%20pesanan:%20...%0AKapan%20diantar:%20...%0A%0ATerimakasih%20kak", "_blank");
+}
+
+function tombol2() {
+     window.open("https://wa.me/628136683171?text=Halo%20kak,%20saya%20tertarik%20dengan%20produk%20kakak.%0ASaya%20ingin%20memesan%20produk%20kakak%20ya.%0A%0ANama:%20...%0AAlamat:%20...%0AJumlah%20pesanan:%20...%0AKapan%20diantar:%20...%0A%0ATerimakasih%20kak", "_blank");
+}
+
+function tombol3() {
+     window.open("https://wa.me/628136683171?text=Halo%20kak,%20saya%20tertarik%20dengan%20produk%20kakak.%0ASaya%20ingin%20memesan%20produk%20kakak%20ya.%0A%0ANama:%20...%0AAlamat:%20...%0AJumlah%20pesanan:%20...%0AKapan%20diantar:%20...%0A%0ATerimakasih%20kak", "_blank");
+}
+
+function tombol4() {
+     window.open("https://wa.me/628136683171?text=Halo%20kak,%20saya%20tertarik%20dengan%20produk%20kakak.%0ASaya%20ingin%20memesan%20produk%20kakak%20ya.%0A%0ANama:%20...%0AAlamat:%20...%0AJumlah%20pesanan:%20...%0AKapan%20diantar:%20...%0A%0ATerimakasih%20kak", "_blank");
+}
+
+function tombol5() {
+     window.open("https://wa.me/628136683171?text=Halo%20kak,%20saya%20tertarik%20dengan%20produk%20kakak.%0ASaya%20ingin%20memesan%20produk%20kakak%20ya.%0A%0ANama:%20...%0AAlamat:%20...%0AJumlah%20pesanan:%20...%0AKapan%20diantar:%20...%0A%0ATerimakasih%20kak", "_blank");
+}
